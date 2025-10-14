069508e763c850511fc4db3e63080bc36878dba7
JGomez1612 <jostgomez1612@gmail.com>
2025-10-14 14:29:16 -0400

    Initial Commit. Boiler Plate.

---
+++ server.mjs
@@ -0,0 +1,17 @@
+// Imports
+import express from "express";
+
+// Setup
+const PORT = 3000;
+const app = express();
+
+// Middleware
+
+// Routes
+
+// Global Error Handling
+
+// Listener
+app.listen(PORT, () => {
+    console.log(`Server Running on PORT: ${PORT}`);
+});
